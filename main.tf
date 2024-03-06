38c17824776c56ac364de3cc779611fc08cf171d
Thais Tozatto <thais.c.tozatto@gmail.com>
2024-03-05 22:30:32 -0300

    fix

--- main.tf
+++ main.tf
@@ -2,18 +2,19 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "4.52.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "3.4.3"
+      version = "~> 5.0"
     }
   }
-  required_version = ">= 1.1.0"
 }
 
+# Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
+}
+
+# Create a VPC
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
 }
 
 resource "random_pet" "sg" {}
