66ee09d2e9458112e4dc68a88c784b58532f869b
t-tozatto <58403079+t-tozatto@users.noreply.github.com>
2024-03-07 20:36:48 -0300

    eks

---
+++ cluster/eks/eks.tf
@@ -0,0 +1,49 @@
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+
+}
+
+module "eks-kubeconfig" {
+  source     = "hyperbadger/eks-kubeconfig/aws"
+  version    = "1.0.0"
+
+  depends_on = [module.eks]
+  cluster_id =  module.eks.cluster_id
+  }
+
+resource "local_file" "kubeconfig" {
+  content  = module.eks-kubeconfig.kubeconfig
+  filename = "kubeconfig_${var.cluster_name}"
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "18.30.3"
+
+  cluster_name    = "eks-${var.cluster_name}"
+  cluster_version = "1.24"
+  subnet_ids        = module.vpc.private_subnets
+  vpc_id = module.vpc.vpc_id
+
+  eks_managed_node_groups = {
+    first = {
+      desired_capacity = 1
+      max_capacity     = 10
+      min_capacity     = 1
+
+      instance_type = var.instance_type
+    }
+  }
+  node_security_group_additional_rules = {
+    ingress_allow_access_from_control_plane = {
+      type                          = "ingress"
+      protocol                      = "tcp"
+      from_port                     = 9443
+      to_port                       = 9443
+      source_cluster_security_group = true
+      description                   = "Allow access from control plane to webhook port of AWS load balancer controller"
+    }
+  }
+}
