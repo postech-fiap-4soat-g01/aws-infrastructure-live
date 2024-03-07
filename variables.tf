474cc0acc232df548c09b22b8e3e2a493a0e4fe3
t-tozatto <58403079+t-tozatto@users.noreply.github.com>
2024-03-07 08:56:43 -0300

    feat: inital eks

--- variables.tf
+++ variables.tf
@@ -1,1 +1,15 @@
+variable "organization" {
+  type    = string
+  default = "postech_soat"
+}
+
+variable "workspace" {
+  type    = string
+  default = "aws-infrastructure-live"
+}
+
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
 
