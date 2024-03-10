55bc1600d96f66e26ca12cb6c942d5e16c8d549b
t-tozatto <58403079+t-tozatto@users.noreply.github.com>
2024-03-10 14:12:50 -0300

    fix

--- cluster/eks/route.tf
+++ cluster/eks/route.tf
@@ -5,6 +5,18 @@ resource "aws_route_table" "private" {
     {
       cidr_block                 = "0.0.0.0/0"
       nat_gateway_id             = aws_nat_gateway.nat.id
+      carrier_gateway_id         = ""
+      destination_prefix_list_id = ""
+      egress_only_gateway_id     = ""
+      gateway_id                 = ""
+      instance_id                = ""
+      ipv6_cidr_block            = ""
+      local_gateway_id           = ""
+      network_interface_id       = ""
+      transit_gateway_id         = ""
+      vpc_endpoint_id            = ""
+      vpc_peering_connection_id  = ""
+      core_network_arn           = ""
     },
   ]
 
@@ -20,6 +32,18 @@ resource "aws_route_table" "public" {
     {
       cidr_block                 = "0.0.0.0/0"
       gateway_id                 = aws_internet_gateway.net_gateway.id
+      nat_gateway_id             = ""
+      carrier_gateway_id         = ""
+      destination_prefix_list_id = ""
+      egress_only_gateway_id     = ""
+      instance_id                = ""
+      ipv6_cidr_block            = ""
+      local_gateway_id           = ""
+      network_interface_id       = ""
+      transit_gateway_id         = ""
+      vpc_endpoint_id            = ""
+      vpc_peering_connection_id  = ""
+      core_network_arn           = ""
     },
   ]
 
