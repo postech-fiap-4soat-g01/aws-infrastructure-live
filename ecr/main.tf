5b130ec6f509a21c9c7659373bdce1921eebdf60
t-tozatto <58403079+t-tozatto@users.noreply.github.com>
2024-03-10 11:34:02 -0300

    fix

--- ecr/main.tf
+++ ecr/main.tf
@@ -1,6 +1,5 @@
 resource "aws_ecr_repository" "ecr" {
-  for_each             = toset(var.ecr_name)
-  name                 = each.key
+  name                 = var.ecr_name
   image_tag_mutability = var.image_mutability
   encryption_configuration {
     encryption_type = var.encrypt_type
