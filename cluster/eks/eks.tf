26eb00d0463a6e30653c147530c347ed863b3101
t-tozatto <58403079+t-tozatto@users.noreply.github.com>
2024-03-10 17:45:49 -0300

    fix subnet cluster

--- cluster/eks/eks.tf
+++ cluster/eks/eks.tf
@@ -27,9 +27,7 @@ resource "aws_eks_cluster" "eks-cluster" {
   role_arn = aws_iam_role.eks-iam.arn
 
   vpc_config {
-    subnet_ids = [
-      aws_subnet.eks-subnet.*.id
-    ]
+    subnet_ids = aws_subnet.eks-subnet.*.id
   }
 
   depends_on = [aws_iam_role_policy_attachment.eks-AmazonEKSClusterPolicy]
