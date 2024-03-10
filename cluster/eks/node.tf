7f6e4d276638241a3bc78ce556b4dcc0ba398aec
t-tozatto <58403079+t-tozatto@users.noreply.github.com>
2024-03-10 17:39:37 -0300

    fix dot

--- cluster/eks/node.tf
+++ cluster/eks/node.tf
@@ -34,7 +34,7 @@ resource "aws_eks_node_group" "private-nodes" {
   node_role_arn   = aws_iam_role.nodes.arn
 
     subnet_ids = [
-    aws_subnet.[*].id
+    aws_subnet[*].id
   ]
 
   capacity_type  = "ON_DEMAND"
