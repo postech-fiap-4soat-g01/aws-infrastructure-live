d4a8ed089f294767cf1c00c1c873889e3cc7239a
t-tozatto <58403079+t-tozatto@users.noreply.github.com>
2024-03-10 10:31:57 -0300

    fix

--- cluster/eks/eks.tf
+++ cluster/eks/eks.tf
@@ -23,14 +23,4 @@ module "eks" {
       instance_type = var.instance_type
     }
   }
-  node_security_group_additional_rules = {
-    ingress_allow_access_from_control_plane = {
-      type                          = "ingress"
-      protocol                      = "tcp"
-      from_port                     = 9443
-      to_port                       = 9443
-      source_cluster_security_group = true
-      description                   = "Allow access from control plane to webhook port of AWS load balancer controller"
-    }
-  }
 }
--- rds/vpc.tf
+++ rds/vpc.tf
@@ -5,7 +5,7 @@ resource "aws_vpc" "rds_vpc" {
 resource "aws_subnet" "rds_subnet_a" {
   vpc_id     = aws_vpc.rds_vpc.id
   cidr_block = "10.0.1.0/24"
-  availability_zone = " us-east-1a"
+  availability_zone = "us-east-1a"
 }
 
 resource "aws_subnet" "rds_subnet_b" {
