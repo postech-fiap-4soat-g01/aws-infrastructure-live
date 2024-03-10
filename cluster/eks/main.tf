3552b6a85741e73465ce0e886d59a68a05cb9108
t-tozatto <58403079+t-tozatto@users.noreply.github.com>
2024-03-10 11:07:06 -0300

    Feat/test (#13)
    * fix

--- cluster/eks/main.tf
+++ cluster/eks/main.tf
@@ -1,9 +1,28 @@
-data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_id
-}
-
-data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_id
-}
-
 data "aws_availability_zones" "available" {}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
+
+  cluster_name    = "eks-${var.cluster_name}"
+  cluster_version = "1.29"
+  subnet_ids      = module.vpc.private_subnets
+  vpc_id          = module.vpc.vpc_id
+
+  eks_managed_node_groups = {
+    first = {
+      desired_capacity = 1
+      max_capacity     = 10
+      min_capacity     = 1
+
+      instance_type = var.instance_type
+    }
+  }
+}
