29060f66cd517b8eae2a52f21ebcde369ef96bee
t-tozatto <58403079+t-tozatto@users.noreply.github.com>
2024-03-11 19:38:38 -0300

    fix

--- dynamodb/main.tf
+++ dynamodb/main.tf
@@ -1,34 +1,9 @@
 resource "aws_dynamodb_table" "user_table" {
- name = "FastFoodUserManagement"
+ name = "user"
  billing_mode = "PAY_PER_REQUEST"
  
  attribute {
   name = "pk"
-  type = "S"
- }
-
- attribute {
-  name = "sk"
-  type = "S"
- }
-
- attribute {
-  name = "name"
-  type = "S"
- }
-
- attribute {
-  name = "email"
-  type = "S"
- }
-
- attribute {
-  name = "identification"
-  type = "S"
- }
-
- attribute {
-  name = "cognitoUserIdentification"
   type = "S"
  }
 
