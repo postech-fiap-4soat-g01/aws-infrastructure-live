0771ef7bbbbe6f56fdf9a0be7610fa029e424088
t-tozatto <58403079+t-tozatto@users.noreply.github.com>
2024-03-11 20:58:21 -0300

    fix rds/eks (#18)

--- cluster_rds/main.tf
+++ cluster_rds/main.tf
@@ -59,3 +59,27 @@ module "eks" {
     }
   }
 }
+
+resource "aws_db_instance" "rds-mssql" {
+  engine            = "sqlserver-ex"
+  engine_version    = "15.00"
+  instance_class    = "db.t3.large"
+  identifier        = "mydb"
+  username          = "dbuser"
+  password          = "dbpassword"
+
+  allocated_storage     = 20
+  max_allocated_storage = 100
+
+  port = 1433
+
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+  db_subnet_group_name   = aws_db_subnet_group.rds_db_subnet_group.name
+
+  skip_final_snapshot = true
+}
+
+resource "aws_db_subnet_group" "rds_db_subnet_group" {
+  name       = "rds-db-subnet-group"
+  subnet_ids = module.vpc.private_subnets
+}
--- cluster_rds/vpc.tf
+++ cluster_rds/vpc.tf
@@ -31,3 +31,17 @@ module "vpc" {
     "kubernetes.io/role/internal-elb"             = 1
   }
 }
+
+resource "aws_security_group" "rds_sg" {
+  name_prefix = "rds-"
+
+  vpc_id = module.vpc.vpc_id
+
+  # Add any additional ingress/egress rules as needed
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
