4a28896c9e3bd87937a3e3fb0d81e25b282ed6e0
Fernando Melim <fernando.lief@hotmail.com>
2024-03-13 01:49:01 -0300

    Removendo output

--- authentication_lambda_access_key/main.tf
+++ authentication_lambda_access_key/main.tf
@@ -30,7 +30,3 @@ resource "aws_iam_user_policy" "fast_food_user_management_lambda_user_policy" {
   user   = aws_iam_user.fast_food_user_management_lambda_user.name
   policy = data.aws_iam_policy_document.fast_food_user_management_lambda_user_policy.json
 }
-
-output "secret" {
-  value = aws_iam_access_key.fast_food_user_management_user_access.encrypted_secret
-}
