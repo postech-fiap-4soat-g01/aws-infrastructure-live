4052883da06f20387b33dee33fc7d30ebe024e41
t-tozatto <thais.c.tozatto@gmail.com>
2024-03-16 00:11:17 -0300

    test

--- cluster_rds/main.tf
+++ cluster_rds/main.tf
@@ -87,10 +87,10 @@ resource "aws_db_subnet_group" "rds_db_subnet_group" {
 
 resource "aws_security_group_rule" "allow-workers-nodes-communications" {
   description              = "Allow worker nodes to communicate with database"
-  from_port                = 5432
+  from_port                = 3306
   protocol                 = "tcp"
   security_group_id        = "${aws_security_group.rds_sg.id}"
   source_security_group_id = "${module.eks.worker_security_group_id}"
-  to_port                  = 5432
+  to_port                  = 3306
   type                     = "ingress"
 }
