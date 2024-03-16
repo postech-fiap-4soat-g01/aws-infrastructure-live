e2c48c2fac8a2d27b679112c6493e32c30b2cdd0
t-tozatto <thais.c.tozatto@gmail.com>
2024-03-15 22:20:06 -0300

    fix

--- main.tf
+++ main.tf
@@ -55,7 +55,7 @@ module "lambda" {
   cognito_user_pool_id        = module.cognito.cognito_user_pool_id
   cognito_user_pool_client_id = module.cognito.cognito_user_pool_client_id
   lambda_role                 = module.authentication_lambda_access_key.lambda_role
-  lambda_role                 = aws_ecr_repository.ecr_user.repository_url
+  ecr_user_repository_url     = aws_ecr_repository.ecr_user.repository_url
 }
 
 module "api_gateway" {
