8af7e4b928fb829884a2faccfe4f6da1a9155971
t-tozatto <thais.c.tozatto@gmail.com>
2024-03-17 09:39:53 -0300

    test

--- variables.tf
+++ variables.tf
@@ -5,11 +5,11 @@ variable "region" {
 
 variable "create_lambda" {
   type    = bool
-  default = false
+  default = true
 }
 
 variable "integration_uri_lb" {
   type    = string
-  default = ""
+  default = "http://ac6b7b0fb8db54e2da01a8caa2b85005-823164483.us-east-1.elb.amazonaws.com"
 }
 
